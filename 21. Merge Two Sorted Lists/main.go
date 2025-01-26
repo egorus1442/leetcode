4936bdefd0ac94a8f635c8209ed7318aaf30ba09
egorus1442 <karmishen@inbox.ru>
2025-01-26 19:09:19 +0300

    mergeTwoLists

---
+++ 21. Merge Two Sorted Lists/main.go
@@ -0,0 +1,30 @@
+package main
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func mergeTwoLists(list1 *ListNode, list2 *ListNode) *ListNode {
+	tail := &ListNode{}
+	head := tail
+
+	for list1 != nil && list2 != nil {
+		if list1.Val < list2.Val {
+			head.Next = list1
+			list1 = list1.Next
+		} else {
+			head.Next = list2
+			list2 = list2.Next
+		}
+		head = head.Next
+	}
+
+	if list1 != nil {
+		head.Next = list1
+	} else {
+		head.Next = list2
+	}
+
+	return tail.Next
+}
